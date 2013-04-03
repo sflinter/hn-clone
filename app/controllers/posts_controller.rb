deabe84b0da374c09eaa3f3c162be11856199abb
Steve Flinter <stephen@flinter.com>
2013-04-03 22:23:04 +0100

    added up voting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.ordered_by_votes
   end
+
+  def update
+    post = Post.find(params[:id])
+    post.up_vote
+    redirect_to posts_path
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   attr_accessible :poster, :title, :url, :votes
 
   scope :ordered_by_votes, order("votes DESC")
+
+  def up_vote
+    self.votes += 1
+    save
+  end
 end
