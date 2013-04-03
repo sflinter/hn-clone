8c2c37c91dfba093fa8b4b8a71fd36cd53b3093b
Steve Flinter <stephen@flinter.com>
2013-04-03 23:27:37 +0100

    allow users to submit news

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,28 @@ class PostsController < ApplicationController
     @posts = Post.ordered_by_votes
   end
 
+  def new
+  end
+
+  def create
+    Post.create(:title => params[:title],
+                :url => urlify(params[:url]),
+                :poster => params[:poster],
+                :votes => 0)
+
+    redirect_to posts_path
+  end
+
   def update
     post = Post.find(params[:id])
     post.up_vote
     redirect_to posts_path
+  end
+
+  private
+  def urlify(url)
+    url.strip!
+    url.insert(0, "http://") unless url =~ /^http:\/\//
+    url
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :poster, :title, :url, :votes
 
+  validates :title, :url, :poster, presence: true
+
   scope :ordered_by_votes, order("votes DESC")
 
   def up_vote
