0971173e439b38898f04ad6b53082797fd1cec86
Steve Flinter <stephen@flinter.com>
2013-04-04 21:57:27 +0100

    added some additional specs testing create and update posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -13,4 +13,34 @@ describe PostsController do
       expect(response).to render_template("index")
     end
   end
+
+  describe 'POST #create' do
+    # let(:new_post) { FactoryGirl.build :post }
+
+    it "creates a new news post" do
+      expect{
+        post :create, FactoryGirl.attributes_for(:post)
+      }.to change(Post, :count).by(1)
+    end
+
+    it "redirects to the homepage" do
+      post :create, FactoryGirl.attributes_for(:post)
+      response.should redirect_to posts_url
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:new_post) { FactoryGirl.create(:post, :votes => 10) }
+
+    it "allows for an up-vote" do
+      put :update, id: new_post
+      new_post.reload
+      new_post.votes.should eq(11)
+    end
+
+    it "redirects to the homepage" do
+      put :update, id: new_post
+      response.should redirect_to posts_url
+    end
+  end
 end
